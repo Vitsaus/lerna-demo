7bca308f9b9fae893f49f052bd9a3eb8ae71a26e
Ville Takala <ville.i.takala@gmail.com>
2020-04-10 14:55:36 +0300

    configs

--- packages/app-a/src/App.tsx
+++ packages/app-a/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import {Button} from '@opr-finance/component-button';
 import {Box} from "@opr-finance/component-box";
+import {Hello} from "@opr-finance/feature-b";
+import {HelloWorld} from "@opr-finance/feature-a";
 
 function App() {
   return (
         <Box>
-            <Box>Simple box</Box>
+            <Box>
+                <Hello />
+            </Box>
+            <Box>
+                <HelloWorld />
+            </Box>
             <Box>
                 <Button
                     onClick={() => {
